87807dd4aac1e02a57365d693d2255fff6c5429a
yunseongyda <kys070510@gmail.com>
2025-02-13 15:46:55 +0900

    begin project

---
+++ Main/src/test/java/com/metacraft/store/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metacraft.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
