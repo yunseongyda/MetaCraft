31001e0c593dbd1fa250ad37d82b22d34c4a882f
gydnjs <khw33352182@gmail.com>
2025-02-24 20:02:15 +0900

    feat : change inquire page
    로그인 한 사용자만 문의를 보낼 수 있도록 수정
    로그인을 하지 않았을 경우 인풋태그 안 내용이 pls login으로 바뀌고,  안에 내용을 입력할 수 없게 된다.
    로그인을 한 경우 email부분의 자신의 이메일이 자동으로 적힌다.

--- src/main/java/com/metacraft/assetstore/Entities/Controller/InquiryController.java
+++ src/main/java/com/metacraft/assetstore/Entities/Controller/InquiryController.java
@@ -1,29 +1,41 @@
 package com.metacraft.assetstore.Entities.Controller;
 
+import java.security.Principal;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
+import com.metacraft.assetstore.Entities.SiteUser;
 import com.metacraft.assetstore.Entities.Service.EmailService;
+import com.metacraft.assetstore.Entities.Service.SiteUserService;
+
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 
 
 @Controller
 @RequiredArgsConstructor
-public class ContactController {
+@RequestMapping("/inquire")
+public class InquiryController {
 
   private final EmailService emailService;
-  @GetMapping("/contact")
-  public String conatact() {
-      return "contact"; 
+  private final SiteUserService siteuserService;
+
+  @GetMapping("")
+  public String contact(Principal principal, Model model) {
+    if (principal != null){
+      SiteUser user = siteuserService.getSiteUser(principal.getName());
+      model.addAttribute("user", user);
+    }
+    return "inquire"; 
   }
 
 
-  @PostMapping("/contact")
+  @PostMapping("/send")
   public String contact(@RequestParam("subject") String subject, @RequestParam("content") String content, @RequestParam("email") String userEmail) {
       emailService.sendEmail(userEmail, subject, content);
 
