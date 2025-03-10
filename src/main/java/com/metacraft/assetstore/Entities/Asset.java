9c91e307a3cab2d0d23da9b9880ba3df8fb68276
yunseongyda <kys070510@gmail.com>
2025-03-10 21:00:36 +0900

    feat : add column for images on Asset Entity
    이미지 저장을 위한 열 추가

--- src/main/java/com/metacraft/assetstore/Entities/Asset.java
+++ src/main/java/com/metacraft/assetstore/Entities/Asset.java
@@ -1,7 +1,13 @@
 package com.metacraft.assetstore.Entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +17,7 @@ import lombok.Setter;
 public class Asset {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   
   private String obj;
@@ -18,6 +25,9 @@ public class Asset {
   private String mtl;
   
   private String bd;
+
+  @OneToMany(mappedBy = "asset", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Image> images; // 하나의 Asset이 여러 개의 이미지를 가질 수 있도록 설정
 
   //todo : 질감마다 얼마나 사용했느지 
 }
