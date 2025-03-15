fe52c3ab5ccacf0f91638870695e9ee890fda2f2
gydnjs <khw33352182@gmail.com>
2025-03-15 16:35:56 +0900

    feat : add UnityWeb build result
    유니티 웹 빌드한 결과물을 프로젝트에 넣었다.
    todo : CreateAsset.html500에러 뜨는 거 해결

--- src/main/java/com/metacraft/assetstore/Entities/Controller/AssetController.java
+++ src/main/java/com/metacraft/assetstore/Entities/Controller/AssetController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.metacraft.assetstore.Entities.Asset;
@@ -15,10 +14,9 @@ import com.metacraft.assetstore.Entities.Service.AssetService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 @RequestMapping("/api/assets")
 @RequiredArgsConstructor
 public class AssetController {
