e6fb15db9bcde0a6a743a339d776a1379966b334
yunseongyda <kys070510@gmail.com>
2025-03-27 19:11:11 +0900

    test : floating image on page
    DB에 있는 url 받아와서 화면에 이미지 보여줌
    todo - 버튼 클릭하면 이미지 다운받기

--- src/main/java/com/metacraft/assetstore/Entities/Controller/ImageController.java
+++ src/main/java/com/metacraft/assetstore/Entities/Controller/ImageController.java
@@ -2,29 +2,24 @@ package com.metacraft.assetstore.Entities.Controller;
 
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.metacraft.assetstore.Entities.Service.ImageService;
 
 import lombok.RequiredArgsConstructor;
 
-@Controller
-@RequestMapping("/test")
+@RestController
+@RequestMapping("/images")
 @RequiredArgsConstructor
-public class TestContoller {
+public class ImageController {
 
   private final ImageService imgService;
 
-  @GetMapping("/lab")
-  public String lab() {
-    return "test";
-  }
-
-  @GetMapping("/searchImage")
+  @GetMapping("/getUrl")
   public String getImageUrls(@RequestParam("num") Integer assetId, Model model) {
     List<String> imageUrls = imgService.getImageUrlsByAssetId(assetId);
     model.addAttribute("imageUrls", imageUrls);
