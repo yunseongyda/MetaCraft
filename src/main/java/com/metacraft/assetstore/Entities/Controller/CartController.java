ea5115012416f55f08eb6834bd4771ec081123d4
gydnjs <khw33352182@gmail.com>
2025-04-26 17:15:08 +0900

    feat: implement add-to-cart logic
    에셋을 장바구니에 넣는 기능을 만들었다

--- src/main/java/com/metacraft/assetstore/Entities/Controller/CartController.java
+++ src/main/java/com/metacraft/assetstore/Entities/Controller/CartController.java
@@ -1,18 +1,68 @@
 package com.metacraft.assetstore.Entities.Controller;
 
+import java.security.Principal;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.metacraft.assetstore.Entities.Cart;
+import com.metacraft.assetstore.Entities.SiteUser;
+import com.metacraft.assetstore.Entities.Service.CartService;
+import com.metacraft.assetstore.Entities.Service.ProductService;
+import com.metacraft.assetstore.Entities.Service.SiteUserService;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/cart")
 public class CartController {
+  private final SiteUserService userService;
+  private final CartService cartService;
+  private final ProductService productService;
+  //장바구니 페이지로 이동하는 메소드
+  @PreAuthorize("isAuthenticated()")
+  @GetMapping("/list")
+  public String cart(Principal principal, Model model) {
 
-  @GetMapping("")
-  public String cart() {
+    SiteUser user = userService.getSiteUser(principal.getName());
+    Cart cart = cartService.getCart(user.getId());
+
+    if (cart == null) {
+      cart = cartService.createCart(user.getId());
+    }
+    model.addAttribute("assetList", cart.getAsset());
+    model.addAttribute("total", 0);
     return "cart";
-  }  
+  } 
+  @PreAuthorize("isAuthenticated()")
+  @GetMapping("/add/{p_id}")
+  public String addAssetToCart(@PathVariable("p_id") Integer id, Principal principal) {
+
+    SiteUser user = userService.getSiteUser(principal.getName());
+    Cart cart = cartService.getCart(user.getId());
+    if (cart == null) {
+      cart = cartService.createCart(user.getId());
+    }
+    cartService.addAssetToCart(cart, productService.getProduct(id));
+    return "redirect:/cart/list";
+  }
+  @PreAuthorize("isAuthenticated()")
+  @GetMapping("/remove/{p_id}")
+  public String deleteAssetFromCart(@PathVariable("p_id") Integer id, Principal principal) {
+
+    SiteUser user = userService.getSiteUser(principal.getName());
+    Cart cart = cartService.getCart(user.getId());
+    if (cart == null) {
+      cart = cartService.createCart(user.getId());
+    }
+    cartService.deleteAssetFromCart(cart, productService.getProduct(id));
+    return "redirect:/cart/list";
+  }
 }
--- src/main/java/com/metacraft/assetstore/Entities/Service/CartService.java
+++ src/main/java/com/metacraft/assetstore/Entities/Service/CartService.java
@@ -1,8 +1,11 @@
 package com.metacraft.assetstore.Entities.Service;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Service;
 
 import com.metacraft.assetstore.Entities.Cart;
+import com.metacraft.assetstore.Entities.Product;
 import com.metacraft.assetstore.Entities.Repository.CartRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,4 +14,25 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class CartService {
 	private final CartRepository cartRepo;
+
+	public Cart getCart(Integer id) {
+		return cartRepo.findById(id).orElse(null);
+	}
+	public Cart createCart(Integer id) {
+		Cart cart = new Cart();
+		cart.setUserId(id);
+		cartRepo.save(cart);
+		return cart;
+	}
+	public void addAssetToCart(Cart cart, Product product) {
+		if (cart.getAsset() == null) {
+			cart.setAsset(new ArrayList<>());
+		}
+		cart.getAsset().add(product);
+		cartRepo.save(cart);
+	}
+	public void deleteAssetFromCart(Cart cart, Product product) {
+		cart.getAsset().remove(product);
+		cartRepo.save(cart);
+	}
 }
