3e04f554f1212f911e0b2626429c2fa3fdcb3bb4
gydnjs <khw33352182@gmail.com>
2025-05-02 16:48:43 +0900

    feat : implement buy products in cart
    장바구니 안에 제품을 일괄적으로 사는 기능 만들었다.

--- src/main/java/com/metacraft/assetstore/Entities/Controller/CartController.java
+++ src/main/java/com/metacraft/assetstore/Entities/Controller/CartController.java
@@ -1,6 +1,7 @@
 package com.metacraft.assetstore.Entities.Controller;
 
 import java.security.Principal;
+import java.util.ArrayList;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.metacraft.assetstore.Entities.Cart;
+import com.metacraft.assetstore.Entities.Product;
 import com.metacraft.assetstore.Entities.SiteUser;
 import com.metacraft.assetstore.Entities.Service.CartService;
 import com.metacraft.assetstore.Entities.Service.ProductService;
 import com.metacraft.assetstore.Entities.Service.SiteUserService;
 
+import groovyjarjarantlr4.v4.parse.BlockSetTransformer.setAlt_return;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -87,6 +90,22 @@ public class CartController {
       cart = cartService.createCart(user.getId());
     }
     cartService.deleteAssetFromCart(cart, productService.getProduct(id));
+    return "redirect:/cart/list";
+  }
+
+  @PreAuthorize("isAuthenticated()")
+  @GetMapping("/buyAll")
+  public String buyAllAssetFromCart(Principal principal) {
+
+    SiteUser user = userService.getSiteUser(principal.getName());
+    Cart cart = cartService.getCart(user.getId());
+    if (user.getProducts() == null) {
+      user.setProducts(new ArrayList<>());
+    }
+    for (Product product : cart.getAsset()) {
+      user.getProducts().add(product);
+      cartService.deleteAssetFromCart(cart, product);
+    }
     return "redirect:/cart/list";
   }
 }
